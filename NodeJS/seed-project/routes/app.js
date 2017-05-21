a9ed082589b58695e68accd1706056e0d3835eb5
Tu Ha <tuha263@gmail.com>
2017-05-21 12:38:39 +0700

    Router and passing data

--- NodeJS/seed-project/routes/app.js
+++ NodeJS/seed-project/routes/app.js
@@ -5,14 +5,21 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/message', function(req, res, next) {
-  res.render('node', {
-    message: 'Hello!'
-  });
+router.get('/message/:msg', function(req, res, next) {
+  if (req.params.msg == null) {
+    res.render('node', {
+      message: 'null'
+    });
+  } else {
+    res.render('node', {
+      message: req.params.msg
+    });
+  }
 });
 
 router.post('/message', function(req, res, next) {
-
+  var message = req.body.message;
+  res.redirect('/message/' + message);
 });
 
 module.exports = router;
