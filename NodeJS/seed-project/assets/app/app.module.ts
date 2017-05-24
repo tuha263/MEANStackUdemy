f486b0d663b5450f218cc4c56827697c37179792
Hoàng Anh Tú <tuha263@gmail.com>
2017-05-24 09:13:36 +0700

    Section 4, lecture 50

--- NodeJS/seed-project/assets/app/app.module.ts
+++ NodeJS/seed-project/assets/app/app.module.ts
@@ -1,10 +1,13 @@
+import { HeaderComponent } from './header.component';
+import { AuthenticationComponent } from './auth/Authentication.component';
+import { MessagesComponent } from './messages/messages.component';
 import { MessageInputComponent } from './messages/message-input.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { MessageComponent } from "./messages/message.component";
+import { AppComponent } from './app.component';
+import {MessageComponent} from './messages/message.component';
 import { MessageListComponent } from './messages/message-list.component';
 
 @NgModule({
@@ -12,7 +15,10 @@ import { MessageListComponent } from './messages/message-list.component';
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
