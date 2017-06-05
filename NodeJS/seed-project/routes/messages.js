c397f13d6970af7d526bf5eff7e77758d0363eb8
Hoàng Anh Tú <tuha263@gmail.com>
2017-06-05 16:30:27 +0700

    section 6, lecture 77

--- NodeJS/seed-project/routes/messages.js
+++ NodeJS/seed-project/routes/messages.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 var Message = require('../models/message');
 
-router.get('/', function(req, res, nex){
+router.get('/', function(req, res, nex) {
     Message.find()
-        .exec(function(err, messages){
-            if (err){
+        .exec(function(err, messages) {
+            if (err) {
                 return res.status(500).json({
                     title: 'An error occurred',
                     error: err
@@ -19,12 +20,24 @@ router.get('/', function(req, res, nex){
         });
 });
 
-router.post('/', function (req, res, next){
+router.use('/', function(req, res, next) {
+    jwt.verify(req.query.token, 'secret', function(err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                title: 'Not Authenticated',
+                error: err
+            });
+        }
+        next();
+    })
+});
+
+router.post('/', function(req, res, next) {
     var message = new Message({
         content: req.body.content
     });
-    message.save(function(err, result){
-        if (err){
+    message.save(function(err, result) {
+        if (err) {
             return res.status(500).json({
                 title: 'An error occurred',
                 error: err
@@ -37,21 +50,21 @@ router.post('/', function (req, res, next){
     });
 });
 
-router.patch('/:id', function(req, res, next){
-    Message.findById(req.params.id, function(err, message){
-        if (err){
+router.patch('/:id', function(req, res, next) {
+    Message.findById(req.params.id, function(err, message) {
+        if (err) {
             return res.status(500).json({
                 title: 'No Message Found!',
-                error: {message: 'Message not found'}
+                error: { message: 'Message not found' }
             });
         }
         message.content = req.body.content;
-        message.save(function(err, result){
-            if (err){
-            return res.status(500).json({
-                title: 'An error occurred',
-                error: err
-            });
+        message.save(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
             }
             res.status(200).json({
                 message: 'Updated message',
@@ -61,20 +74,20 @@ router.patch('/:id', function(req, res, next){
     });
 });
 
-router.delete('/:id',function(req, res, next){
-     Message.findById(req.params.id, function(err, message){
-        if (err){
+router.delete('/:id', function(req, res, next) {
+    Message.findById(req.params.id, function(err, message) {
+        if (err) {
             return res.status(500).json({
                 title: 'No Message Found!',
-                error: {message: 'Message not found'}
+                error: { message: 'Message not found' }
             });
         }
-        message.remove(function(err, result){
-            if (err){
-            return res.status(500).json({
-                title: 'An error occurred',
-                error: err
-            });
+        message.remove(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
             }
             res.status(200).json({
                 message: 'Deleted message',
